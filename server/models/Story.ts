8ac1c9e930530c40b7021ec17116a29851865493
Sumit0-hash <sumitbhatia900@gmail.com>
2026-09-05 17:07:53 +0530

    Backend completed for Voqra

---
+++ server/models/Story.ts
@@ -0,0 +1,21 @@
+import mongoose, { Model, Schema } from "mongoose";
+
+
+export interface IStory extends Document {
+    user: string;
+    mediaUrl: string;
+    mediaType: "image" | "video";
+    createdAt: Date;
+}
+
+const StorySchema = new Schema<IStory>({
+    user: {type: String, required: true},
+    mediaUrl: {type: String, required: true},
+    mediaType: {type: String, enum: ["image", "video"], required: true},
+    createdAt: {type: Date, default: Date.now, expires: 86400}, //24 hours
+    
+},{timestamps: true})
+
+const Story: Model<IStory> = mongoose.model("Story", StorySchema)
+
+export default Story;
